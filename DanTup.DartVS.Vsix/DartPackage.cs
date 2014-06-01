f9a864fa317d1e3240344384be29b8459166bfe9
Danny Tuppeny <danny@tuppeny.com>
2014-06-01 17:19:16 +0100

    Added empty VS package stuff.

---
+++ DanTup.DartVS.Vsix/DartPackage.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+
+namespace DanTup.DartVS
+{
+	[InstalledProductRegistration("DanTup's Dart support for Visual Studio", @"Some support for coding Dart in Visual Studio.", "0.1")]
+	[ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string)]
+	public sealed class DartPackage : Package
+	{
+	}
+}
---
+++ DanTup.DartVS.Vsix/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DanTup's Dart support for Visual Studio (VSIX)")]
+[assembly: AssemblyProduct("DanTup's Dart support for Visual Studio")]
+[assembly: AssemblyCompany("Danny Tuppeny")]
+[assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
