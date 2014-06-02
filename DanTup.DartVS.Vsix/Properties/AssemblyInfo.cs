c2a1710576e147c9f2c67db1e94b620e1f8bbc37
Danny Tuppeny <danny@tuppeny.com>
2014-06-02 19:50:00 +0100

    Update version to 0.2.

--- DanTup.DartVS.Vsix/Properties/AssemblyInfo.cs
+++ DanTup.DartVS.Vsix/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
