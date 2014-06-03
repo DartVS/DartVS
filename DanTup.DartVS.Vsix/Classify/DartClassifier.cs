d84dae6c2aff671de2666b45402b1bd497bb35f0
Danny Tuppeny <danny@tuppeny.com>
2014-06-03 21:15:20 +0100

    Tweaks to syntax highlighting regexes.
    Issue #4. Found a good list of keywords from the Dart site, and added
    word boundaries to ensure there are no partial matches in the middle of
    words (eg. in a method called resetBlah, the "set" was matching).

--- DanTup.DartVS.Vsix/Classify/DartClassifier.cs
+++ DanTup.DartVS.Vsix/Classify/DartClassifier.cs
@@ -10,8 +10,11 @@ namespace DanTup.DartVS
 {
 	class DartClassifier : IClassifier
 	{
-		private static Regex _rxKeywords = new Regex(@"(var|import|class|super|library|new|with|extends|factory|if|else|throw|static|show)", RegexOptions.Compiled);
-		private static Regex _rxIdentifier = new Regex(@"(int|bool|String|void|num|@\w+)", RegexOptions.Compiled);
+		// https://www.dartlang.org/docs/dart-up-and-running/contents/ch02.html#keyword_table
+		private static Regex _rxKeywords1 = new Regex(@"\b(abstract|continue|extends|implements|part|throw|as|default|factory|import|rethrow|true|assert|do|false|in|return|try|break|dynamic|final|is|set|typedef|case|else|finally|library|static|var|catch|enum|for|new|super|void|class|export|get|null|switch|while|const|external|if|operator|this|with)\b", RegexOptions.Compiled);
+		// https://github.com/dart-lang/dartlang.org/blob/54d221410e04ddf7424400aec473f0a31d08b194/src/site/js/lang-dart.js
+		private static Regex _rxKeywords2 = new Regex(@"\b(interface|native|part of|part|show|hide)\b", RegexOptions.Compiled);
+		private static Regex _rxIdentifier = new Regex(@"\b(bool|double|Dynamic|int|num|Object|String|void)\b|@\w+\b", RegexOptions.Compiled);
 		private static Regex _rxString = new Regex(@"'([^']+)'", RegexOptions.Compiled);
 		private static Regex _rxComment = new Regex("//.*", RegexOptions.Compiled);
 		private Dictionary<Regex, IClassificationType> _map;
@@ -23,7 +26,8 @@ namespace DanTup.DartVS
 				{_rxComment, registry.GetClassificationType(PredefinedClassificationTypeNames.Comment)},
 				{_rxString, registry.GetClassificationType(PredefinedClassificationTypeNames.String)},
 				{_rxIdentifier, registry.GetClassificationType(PredefinedClassificationTypeNames.SymbolDefinition)},
-				{_rxKeywords, registry.GetClassificationType(PredefinedClassificationTypeNames.Keyword)},
+				{_rxKeywords1, registry.GetClassificationType(PredefinedClassificationTypeNames.Keyword)},
+				{_rxKeywords2, registry.GetClassificationType(PredefinedClassificationTypeNames.Keyword)},
 			};
 		}
 
