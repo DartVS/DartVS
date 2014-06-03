1e71d7efa6ddb5cc261eb2d6dcb75dc23e9c948a
Mads Kristensen <post@madskristensen.net>
2014-06-02 18:49:13 -0700

    Added classifier and content type

---
+++ DanTup.DartVS.Vsix/Classify/DartClassifierProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DanTup.DartVS
+{
+    [Export(typeof(IClassifierProvider))]
+    [ContentType(DartContentTypeDefinition.DartContentType)]
+    class DartClassifierProvider : IClassifierProvider
+    {
+        [Import]
+        public IClassificationTypeRegistryService Registry { get; set; }
+
+        public IClassifier GetClassifier(ITextBuffer textBuffer)
+        {
+            return textBuffer.Properties.GetOrCreateSingletonProperty(() => new DartClassifier(Registry));
+        }
+    }
+}
---
+++ DanTup.DartVS.Vsix/ContentType/DartContentTypeDefinition.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DanTup.DartVS
+{
+    class DartContentTypeDefinition
+    {
+        public const string DartContentType = "Dart";
+
+        /// <summary>
+        /// Exports the Dart content type
+        /// </summary>
+        [Export(typeof(ContentTypeDefinition))]
+        [Name(DartContentType)]
+        [BaseDefinition("code")]
+        public ContentTypeDefinition IDartContentType { get; set; }
+
+        [Export(typeof(FileExtensionToContentTypeDefinition))]
+        [ContentType(DartContentType)]
+        [FileExtension(".dart")]
+        public FileExtensionToContentTypeDefinition DartFileExtension { get; set; }
+    }
+}
