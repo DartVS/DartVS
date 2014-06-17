3bfc2757514529f3914e96d5c97fd5a6bacc5060
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 19:20:06 +0100

    Added (empty) main implementation class for consumers.

--- DanTup.DartAnalysis.Tests/ServerTests.cs
+++ DanTup.DartAnalysis.Tests/ServerTests.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Xunit;
 
 namespace DanTup.DartAnalysis.Tests
 {
 	class ServerTests : Tests
 	{
-		//[Fact]
-		//public async Task ServerVersion()
-		//{
-		//	using (var service = new DartAnalysisService(SdkFolder, ServerScript))
-		//	{
-		//	}
-		//}
+		[Fact]
+		public async Task ServerVersion()
+		{
+			using (var service = new DartAnalysisService(SdkFolder, ServerScript))
+			{
+			}
+		}
 	}
 }
---
+++ DanTup.DartAnalysis/DartAnalysisService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanTup.DartAnalysis
+{
+	/// <summary>
+	/// Wraps the Google Dart Analysis Service providing a strongly-typed .NET
+	/// interface.
+	/// </summary>
+	public class DartAnalysisService : IDisposable
+	{
+		readonly AnalysisServiceWrapper service;
+
+		/// <summary>
+		/// Launches the Google Dart Analysis Service using the provided SDK and script.
+		/// </summary>
+		/// <param name="sdkFolder">The location of the Dart SDK.</param>
+		/// <param name="serverScript">The location of the Dart script that runs the Analysis Service.</param>
+		/// <param name="eventHandler">A handler for events raised by the Analysis Service.</param>
+		public DartAnalysisService(string sdkFolder, string serverScript)
+		{
+			service = new AnalysisServiceWrapper(sdkFolder, serverScript);
+		}
+		
+		#region OMG DO WE STILL HAVE TO DO THIS?
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				service.Dispose();
+			}
+		}
+
+		#endregion
+	}
+}
