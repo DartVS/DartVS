f7df28363522d4a8c08a6012c5f3c291fdc327d7
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 19:53:37 +0100

    Added ability to send SetAnalysisRoots.
    Test marked as skip; requires implementation of events for this to work.

---
+++ DanTup.DartAnalysis.Tests/AnalysisTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace DanTup.DartAnalysis.Tests
+{
+	public class AnalysisTests : Tests
+	{
+		[Fact(Skip = "Incomplete test; requires event implementation")]
+		public async Task SetAnalysisRoots()
+		{
+			using (var service = new DartAnalysisService(SdkFolder, ServerScript))
+			{
+				await service.SetAnalysisRoots(new[] { SampleDartProject }, new string[] { });
+
+				// TODO: Wait for some events!
+			}
+		}
+	}
+}
---
+++ DanTup.DartAnalysis/Commands/AnalysisSetAnalysisRoots.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+
+namespace DanTup.DartAnalysis
+{
+	class AnalysisSetAnalysisRootsRequest : Request<AnalysisSetAnalysisRootsParams, Response>
+	{
+		public string method = "analysis.setAnalysisRoots";
+
+		public AnalysisSetAnalysisRootsRequest(string[] included, string[] excluded)
+		{
+			this.@params = new AnalysisSetAnalysisRootsParams(included, excluded);
+		}
+	}
+
+	class AnalysisSetAnalysisRootsParams
+	{
+		public string[] included;
+		public string[] excluded;
+
+		public AnalysisSetAnalysisRootsParams(string[] included, string[] excluded)
+		{
+			this.included = included;
+			this.excluded = excluded;
+		}
+	}
+
+	public static class AnalysisSetAnalysisRootsImplementation
+	{
+		public static async Task SetAnalysisRoots(this DartAnalysisService service, string[] included, string[] excluded)
+		{
+			var response = await service.Service.Send(new AnalysisSetAnalysisRootsRequest(included, excluded));
+
+			// There's nothing useful on this response to return.
+
+			return;
+		}
+	}
+}
