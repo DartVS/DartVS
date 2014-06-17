794cab88143ecf1b556010c9dc54d01c9cf74d7e
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 18:46:47 +0100

    Create empty DLL project.

---
+++ DanTup.DartAnalysis/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DanTup's DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
+[assembly: AssemblyProduct("DanTup's DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
+[assembly: AssemblyCompany("Danny Tuppeny")]
+[assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
