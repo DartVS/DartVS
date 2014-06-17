19f48641f0b7363fb4d37c9762304ff271bbf237
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 20:31:13 +0100

    Added tests for Analysis events.

--- DanTup.DartAnalysis.Tests/AnalysisTests.cs
+++ DanTup.DartAnalysis.Tests/AnalysisTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DanTup.DartAnalysis.Tests
@@ -14,16 +16,28 @@ namespace DanTup.DartAnalysis.Tests
 				bool? isAnalyzing = null;
 				service.ServerStatusNotification += (s, e) => { isAnalyzing = e.IsAnalysing; };
 
+				// Keep track of errors that are reported
+				List<AnalysisError> errors = new List<AnalysisError>();
+				service.AnalysisErrorsNotification += (s, e) => { errors.AddRange(e.Errors); };
+
+
 				// Send a request to do some analysis.
 				await service.SetAnalysisRoots(new[] { SampleDartProject }, new string[] { });
 
 				// Allow time for analysing.
 				await Task.Delay(5000);
 
+
 				// Ensure the event fired to say analysing had finished.
 				Assert.Equal(false, isAnalyzing);
 
-				// TODO: Ensure expected errors came back...
+				// Ensure the error-free file got no errors.
+				Assert.Equal(0, errors.Where(e => e.File.EndsWith("\\hello_world.dart")).Count());
+
+				// Ensure the single-error file got the expected error.
+				Assert.Equal(1, errors.Where(e => e.File.EndsWith("\\single_type_error.dart")).Distinct().Count());
+				var error = errors.First(e => e.File.EndsWith("\\single_type_error.dart"));
+				Assert.Equal("StaticWarningCode.ARGUMENT_TYPE_NOT_ASSIGNABLE", error.ErrorCode);
 			}
 		}
 	}
