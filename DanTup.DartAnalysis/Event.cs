18249028997767765ad135d6c52c43c85114a05e
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 18:55:46 +0100

    Added an Event class for server notifications.
    Used to receive server notifications that are not tied to requests (not
    responses).

---
+++ DanTup.DartAnalysis/Event.cs
@@ -0,0 +1,44 @@
+﻿using System.Web.Script.Serialization;
+
+namespace DanTup.DartAnalysis
+{
+	/// <summary>
+	/// Represents an event raised by the Analysis Service that has
+	/// type parameters.
+	/// </summary>
+	/// <typeparam name="TParams">The type of the paramters for this event.</typeparam>
+	class Event<TParams> : Event
+	{
+		public TParams @params;
+	}
+
+	/// <summary>
+	/// Represents an event raised by the Analysis Service.
+	/// </summary>
+	class Event
+	{
+		public string @event;
+
+		#region Equality checks
+
+		// Since we know these objects are serialisable, this is a quick-but-hacky way of checking for structural equality.
+		// TODO: Make this better (or convert to F#? ;))
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+
+			var serialiser = new JavaScriptSerializer();
+			return serialiser.Serialize(this).Equals(serialiser.Serialize(obj));
+		}
+
+		public override int GetHashCode()
+		{
+			var serialiser = new JavaScriptSerializer();
+			return serialiser.Serialize(this).GetHashCode();
+		}
+
+		#endregion
+	}
+}
