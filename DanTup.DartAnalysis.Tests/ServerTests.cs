4b5ad4e6716459da63a0fe867c6fb352eb15350a
Danny Tuppeny <danny@tuppeny.com>
2014-06-17 19:14:12 +0100

    Added (mostly-empty) test project.

---
+++ DanTup.DartAnalysis.Tests/ServerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DanTup.DartAnalysis.Tests
+{
+	class ServerTests : Tests
+	{
+		//[Fact]
+		//public async Task ServerVersion()
+		//{
+		//	using (var service = new DartAnalysisService(SdkFolder, ServerScript))
+		//	{
+		//	}
+		//}
+	}
+}
---
+++ DanTup.DartAnalysis.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanTup.DartAnalysis.Tests
+{
+	abstract class Tests
+	{
+		protected const string SdkFolder = @"M:\Apps\Dart\SDK";
+		protected const string ServerScript = @"M:\Coding\TestApps\DartServerTest\bin\server.dart";
+		protected const string SampleDartProject = @"M:\Coding\TestApps\DartServerTest\SampleDartProject";
+	}
+}
