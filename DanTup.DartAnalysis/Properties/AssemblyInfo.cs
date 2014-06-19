669c708a14a317addd4003acac94e3f04f4eb8a3
Danny Tuppeny <danny@tuppeny.com>
2014-06-19 08:24:06 +0100

    Remove name from assembly info; this looks silly on NuGet.

--- DanTup.DartAnalysis/Properties/AssemblyInfo.cs
+++ DanTup.DartAnalysis/Properties/AssemblyInfo.cs
@@ -2,11 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DanTup's DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
-[assembly: AssemblyProduct("DanTup's DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
+[assembly: AssemblyTitle("DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
+[assembly: AssemblyProduct("DartAnalysis.NET: A .NET wrapper around Google's Analysis Server for Dart.")]
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("DanTup.DartAnalysis.Tests")]
