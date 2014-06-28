b1b579cd8fbfc1a42a6e8efc2321866e1a45c684
Danny Tuppeny <danny@tuppeny.com>
2014-06-28 12:15:50 +0100

    Update version for push to NuGet.

--- Push NuGet Package.ps1
+++ Push NuGet Package.ps1
@@ -3,8 +3,8 @@ pushd DanTup.DartAnalysis
 # This version will need updating every time; can't see an easy way to set pre-release
 # without maintaining a NuSpec :/
 
-nuget pack -Version 0.1.2-alpha
-nuget push .\DanTup.DartAnalysis.0.1.2-alpha.nupkg
-del .\DanTup.DartAnalysis.0.1.2-alpha.nupkg
+nuget pack
+nuget push *.nupkg
+del *.nupkg
 
 popd
