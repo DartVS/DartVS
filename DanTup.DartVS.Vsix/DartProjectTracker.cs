83c8118d635404e6483af661ec7c0e86608b1d61
Danny Tuppeny <danny@tuppeny.com>
2014-06-30 17:05:02 +0100

    Add support for detecting files that become/stop being Dart projects.

--- DanTup.DartVS.Vsix/DartProjectTracker.cs
+++ DanTup.DartVS.Vsix/DartProjectTracker.cs
@@ -19,6 +19,7 @@ namespace DanTup.DartVS
 		internal SVsServiceProvider ServiceProvider = null;
 
 		ConcurrentDictionary<string, Project> dartProjects = new ConcurrentDictionary<string, Project>();
+		ConcurrentDictionary<string, FileSystemWatcher> openProjectWatchers = new ConcurrentDictionary<string, FileSystemWatcher>();
 
 		[ImportingConstructor]
 		public DartProjectTracker([Import]SVsServiceProvider serviceProvider)
@@ -37,20 +38,37 @@ namespace DanTup.DartVS
 
 		void TrackProject(Project project)
 		{
-			if (!IsDartProject(project))
-				return;
+			var projectFolder = GetProjectLocation(project);
 
-			dartProjects.TryAdd(GetProjectLocation(project), project);
+			openProjectWatchers.TryAdd(projectFolder, CreateWatcher(project));
+
+			if (IsDartProject(project))
+				dartProjects.TryAdd(projectFolder, project);
 		}
 
 		void UntrackProject(Project project)
 		{
+			var projectFolder = GetProjectLocation(project);
+
+			// Remove the FSW
+			FileSystemWatcher watcher;
+			openProjectWatchers.TryRemove(projectFolder, out watcher);
+			if (watcher != null)
+				watcher.Dispose();
+
+			// Untrack the project
 			Project _;
-			dartProjects.TryRemove(GetProjectLocation(project), out _);
+			dartProjects.TryRemove(projectFolder, out _);
 		}
 
 		void UntrackAllProjects()
 		{
+			// Remove all FSWs
+			foreach (var watcher in openProjectWatchers.Values)
+				watcher.Dispose();
+			openProjectWatchers.Clear();
+
+			// Untrack all projects
 			dartProjects.Clear();
 		}
 
@@ -58,6 +76,33 @@ namespace DanTup.DartVS
 		{
 			// This way seems to be reliable for both projects and "web sites" (whereas project.FullName gives URL for web sites).
 			return (string)project.Properties.Item("FullPath").Value;
+		}
+
+		FileSystemWatcher CreateWatcher(Project project)
+		{
+			var watcher = new FileSystemWatcher(GetProjectLocation(project), "*.dart");
+
+			watcher.IncludeSubdirectories = true;
+			watcher.NotifyFilter = NotifyFilters.FileName;
+			watcher.Created += (o, e) => UpdateProject(project, e.FullPath);
+			watcher.Deleted += (o, e) => UpdateProject(project, e.FullPath);
+			watcher.Renamed += (o, e) => UpdateProject(project, e.FullPath);
+
+			watcher.EnableRaisingEvents = true;
+
+			return watcher;
+		}
+
+		void UpdateProject(Project project, string fullPath)
+		{
+			// A file was modified that had/has a .dart extension, so we need to track/untrack accordingly.
+			var isDartProject = IsDartProject(project);
+			var wasDartProject = dartProjects.ContainsKey(GetProjectLocation(project));
+
+			if (isDartProject && !wasDartProject)
+				TrackProject(project);
+			else if (!isDartProject && wasDartProject)
+				UntrackProject(project);
 		}
 
 		bool IsDartProject(Project project)
