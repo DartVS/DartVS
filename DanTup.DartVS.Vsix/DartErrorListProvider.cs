fefcb5ee98f6ae4726f4698f4dee71488ee1e031
Danny Tuppeny <danny@tuppeny.com>
2014-07-02 19:58:12 +0100

    Updates to use service-provided line/col.

--- DanTup.DartVS.Vsix/DartErrorListProvider.cs
+++ DanTup.DartVS.Vsix/DartErrorListProvider.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using System.Linq;
 using DanTup.DartAnalysis;
 using EnvDTE;
@@ -49,8 +48,6 @@ namespace DanTup.DartVS
 
 		private ErrorTask CreateErrorTask(AnalysisError analysisError)
 		{
-			var lineInformaion = GetLineInformation(analysisError.File, analysisError.Offset);
-
 			return new ErrorTask
 			{
 				// TODO: This is bogus; they don't start with this anymore...
@@ -59,70 +56,10 @@ namespace DanTup.DartVS
 					: analysisError.Message.StartsWith("warning") ? TaskErrorCategory.Warning
 					: TaskErrorCategory.Error,
 				Text = analysisError.Message,
-				Document = analysisError.File,
-				// TODO: Figure this out (we have offset + length!)
-				Line = lineInformaion.Line - 1, // Line appears to be 0-based! :-(
-				Column = lineInformaion.Column
+				Document = analysisError.Location.File,
+				Line = analysisError.Location.StartLine - 1, // Line appears to be 0-based in VS! :-(
+				Column = analysisError.Location.StartColumn
 			};
-		}
-
-		/// <summary>
-		/// Gets line information from an offset. The DartAnalysis service always returns offset/lengths, not lines/cols.
-		/// </summary>
-		LineInformation GetLineInformation(string path, int offset)
-		{
-			// Figure out if this file is open; as the contents might be different in the buffer to on disk.
-			var openDocument = dte.Documents.Cast<Document>().FirstOrDefault(d => d.FullName == path);
-
-			// Read the contents from the buffer or from disk, depending on whether the file is open.
-			string fileContents = null;
-			if (openDocument != null)
-			{
-				var doc = openDocument.Object("") as TextDocument;
-				fileContents = doc.CreateEditPoint().GetText(doc.EndPoint);
-			}
-			else
-				fileContents = File.ReadAllText(path);
-
-			// Walk through the file counting the characters in each line until we get to the offset.
-			// TODO: Do we handle the count the same as Google's service for Unicode etc.? Needs some testing...
-			var lines = fileContents.Split('\n');
-			var totalCharacters = 0;
-			for (int l = 0; l < lines.Length; l++)
-			{
-				// If this line ends prior to the offset, add the length and continue.
-				if (totalCharacters + lines[l].Length < offset)
-				{
-					totalCharacters += lines[l].Length;
-					continue;
-				}
-				// If the offset is within this line, return the information.
-				else if (totalCharacters + lines[l].Length >= offset)
-				{
-					// Lines are 1-based!
-					return new LineInformation(l + 1, offset - totalCharacters);
-				}
-				else
-				{
-					// Something has gone wrong :(
-					return new LineInformation(1, 1);
-				}
-			}
-
-			// Something has also gone wrong :(
-			return new LineInformation(1, 1);
-		}
-
-		class LineInformation
-		{
-			public int Line { get; private set; }
-			public int Column { get; private set; }
-
-			public LineInformation(int line, int column)
-			{
-				this.Line = line;
-				this.Column = column;
-			}
 		}
 	}
 }
