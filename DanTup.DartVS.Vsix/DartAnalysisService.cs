b967f7af3bceda7ac0caa3a8403e3a6c6a300b0a
Danny Tuppeny <danny@tuppeny.com>
2014-07-02 22:05:08 +0100

    Subscribe to errors for open files.
    This will force errors to be re-sent, even if we've had them. This will
    be required by the Squiggle tagger, which needs the data after the
    normal analysis has already been run.

--- DanTup.DartVS.Vsix/DartAnalysisService.cs
+++ DanTup.DartVS.Vsix/DartAnalysisService.cs
@@ -12,23 +12,29 @@ namespace DanTup.DartVS
 	[Export]
 	public class DartAnalysisService : DartAnalysis.DartAnalysisService
 	{
-		DartProjectTracker projectTracker;
+		readonly DartProjectTracker projectTracker;
+		readonly OpenFileTracker openFileTracker;
+		readonly AnalysisSubscription[] subscriptions = new[] { AnalysisSubscription.Errors };
 
 		static readonly string SdkPath =
 			Environment.GetEnvironmentVariable("DART_SDK", EnvironmentVariableTarget.User)
 			?? Environment.GetEnvironmentVariable("DART_SDK", EnvironmentVariableTarget.Machine);
 
 		[ImportingConstructor]
-		public DartAnalysisService([Import]DartProjectTracker projectTracker)
+		public DartAnalysisService([Import]DartProjectTracker projectTracker, [Import]OpenFileTracker openFileTracker)
 			: base(SdkPath, @"M:\Coding\TestApps\DartServerTest\bin\server.dart")
 		{
 			this.projectTracker = projectTracker;
+			this.openFileTracker = openFileTracker;
 
 			// TODO: Only fire this up when there's at least one Dart project open!
 			// TODO: Shut it down when the last dart project closes!
 
-			// When Dart projects change; update analysis roots
+			// When Dart projects change; update analysis roots.
 			this.projectTracker.ProjectsChanged.Subscribe(projs => this.SetAnalysisRoots(projs.Select(p => p.Path).ToArray()));
+
+			// When open files change; update subscriptions.
+			this.openFileTracker.DocumentsChanged.Subscribe(files => this.SetAnalysisSubscriptions(subscriptions.ToDictionary(s => s, s => files)));
 		}
 	}
 }
