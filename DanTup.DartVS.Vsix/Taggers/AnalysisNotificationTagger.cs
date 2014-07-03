4a0f68008429ee96362d1f4d6b95a7b728c661dd
Danny Tuppeny <danny@tuppeny.com>
2014-07-03 10:09:45 +0100

    Extract reusable logic out of ErrorTagger into a base Tagger.

---
+++ DanTup.DartVS.Vsix/Taggers/AnalysisNotificationTagger.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Tagging;
+
+namespace DanTup.DartVS.Taggers
+{
+	abstract class AnalysisNotificationTagger<TTag, TSourceData, TNotificationType> : ITagger<TTag> where TTag : ITag
+	{
+		protected ITextBuffer buffer;
+		protected ITextDocumentFactoryService textDocumentFactory;
+		protected ITextDocument textDocument;
+		protected DartAnalysisService analysisService;
+		protected TSourceData[] currentData = new TSourceData[0];
+		int earliestOffset = 0;
+		int latestEnd = 0;
+
+		public AnalysisNotificationTagger(ITextBuffer buffer, ITextDocumentFactoryService textDocumentFactory, DartAnalysisService analysisService)
+		{
+			this.buffer = buffer;
+			this.textDocumentFactory = textDocumentFactory;
+			this.analysisService = analysisService;
+
+			textDocumentFactory.TryGetTextDocument(this.buffer, out this.textDocument);
+
+			// Subscribe to errors for the current file.
+			this.Subscribe(UpdateSourceData);
+		}
+
+		public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
+
+		public IEnumerable<ITagSpan<TTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+		{
+			foreach (var data in currentData)
+			{
+				var location = GetOffsetAndLength(data);
+
+				// VS will crash if we pass an invalid span. Since our error might arrive after more changes are made; we'll have to filter
+				// them out here.
+				if (location.Item1 + location.Item2 > buffer.CurrentSnapshot.Length)
+					continue;
+
+				yield return CreateTag(data);
+			}
+		}
+
+		private void UpdateSourceData(TNotificationType notification)
+		{
+			var dataToTag = GetDataToTag(notification);
+
+			Interlocked.Exchange(ref currentData, dataToTag);
+
+			if (dataToTag.Any())
+			{
+				// Figure out the smallest span that we have errors for.
+				var locationInfo = dataToTag.Select(e => GetOffsetAndLength(e));
+				var newEarliestOffset = locationInfo.Min(e => e.Item1);
+				var newLatestEnd = locationInfo.Max(e => e.Item1 + e.Item2);
+
+				var startPositionToUse = Math.Min(earliestOffset, newEarliestOffset);
+				var endPositionToUse = Math.Max(latestEnd, newLatestEnd);
+				var lengthToUse = Math.Min(endPositionToUse - startPositionToUse, buffer.CurrentSnapshot.Length - startPositionToUse);
+
+				var handler = this.TagsChanged;
+				if (handler != null)
+					// Use the biggest span that includes boh previous and new errors, so we correctly remove any that are gone.
+					handler(this, new SnapshotSpanEventArgs(new SnapshotSpan(buffer.CurrentSnapshot, startPositionToUse, lengthToUse)));
+
+				// Remember for next time round.
+				earliestOffset = newEarliestOffset;
+				latestEnd = newLatestEnd;
+			}
+			else
+			{
+				// No new errors, so just invalidate the old ones.
+				var startPositionToUse = earliestOffset;
+				var endPositionToUse = Math.Min(latestEnd, buffer.CurrentSnapshot.Length);
+				var lengthToUse = Math.Min(endPositionToUse - startPositionToUse, buffer.CurrentSnapshot.Length - startPositionToUse);
+
+				var handler = this.TagsChanged;
+				if (handler != null)
+					// Use the biggest span that includes boh previous and new errors, so we correctly remove any that are gone.
+					handler(this, new SnapshotSpanEventArgs(new SnapshotSpan(buffer.CurrentSnapshot, earliestOffset, lengthToUse)));
+
+				earliestOffset = 0;
+				latestEnd = 0;
+			}
+		}
+
+		protected abstract ITagSpan<TTag> CreateTag(TSourceData data);
+		protected abstract void Subscribe(Action<TNotificationType> updateSourceData);
+		protected abstract TSourceData[] GetDataToTag(TNotificationType notification);
+		protected abstract Tuple<int, int> GetOffsetAndLength(TSourceData data);
+	}
+}
--- DanTup.DartVS.Vsix/Taggers/ErrorSquiggleTagger.cs
+++ DanTup.DartVS.Vsix/Taggers/ErrorSquiggleTagger.cs
@@ -28,85 +28,31 @@ namespace DanTup.DartVS.Taggers
 		}
 	}
 
-	class ErrorSquiggleTagger : ITagger<ErrorTag>
+	class ErrorSquiggleTagger : AnalysisNotificationTagger<ErrorTag, AnalysisError, AnalysisErrorsNotification>
 	{
-		ITextBuffer buffer;
-		ITextDocumentFactoryService textDocumentFactory;
-		ITextDocument textDocument;
-		DartAnalysisService analysisService;
-		AnalysisError[] currentErrors = new AnalysisError[0];
-		int earliestErrorOffset = 0;
-		int latestErrorEnd = 0;
-
 		public ErrorSquiggleTagger(ITextBuffer buffer, ITextDocumentFactoryService textDocumentFactory, DartAnalysisService analysisService)
+			: base(buffer, textDocumentFactory, analysisService)
 		{
-			this.buffer = buffer;
-			this.textDocumentFactory = textDocumentFactory;
-			this.analysisService = analysisService;
-
-			textDocumentFactory.TryGetTextDocument(this.buffer, out this.textDocument);
-
-			// Subscribe to errors for the current file.
-			this.analysisService.AnalysisErrorsNotification.Where(en => en.File == textDocument.FilePath).Subscribe(UpdateErrors);
 		}
 
-		public IEnumerable<ITagSpan<ErrorTag>> GetTags(NormalizedSnapshotSpanCollection spans)
+		protected override ITagSpan<ErrorTag> CreateTag(AnalysisError error)
 		{
-			foreach (var error in currentErrors)
-			{
-				// VS will crash if we pass an invalid span. Since our error might arrive after more changes are made; we'll have to filter
-				// them out here.
-				if (error.Location.Offset + error.Location.Length > buffer.CurrentSnapshot.Length)
-					continue;
-
-				yield return new TagSpan<ErrorTag>(new SnapshotSpan(buffer.CurrentSnapshot, error.Location.Offset, error.Location.Length), new ErrorTag("syntax error", error.Message));
-			}
+			return new TagSpan<ErrorTag>(new SnapshotSpan(buffer.CurrentSnapshot, error.Location.Offset, error.Location.Length), new ErrorTag("syntax error", error.Message));
 		}
 
-		public event EventHandler<SnapshotSpanEventArgs> TagsChanged;
-
-		private void UpdateErrors(AnalysisErrorsNotification notification)
+		protected override void Subscribe(Action<AnalysisErrorsNotification> updateSourceData)
 		{
-			var errorsForThisFile = notification.Errors.Where(e => e.Location.File == textDocument.FilePath).ToArray();
+			this.analysisService.AnalysisErrorsNotification.Where(en => en.File == textDocument.FilePath).Subscribe(updateSourceData);
+		}
 
-			Interlocked.Exchange(ref currentErrors, errorsForThisFile);
+		protected override AnalysisError[] GetDataToTag(AnalysisErrorsNotification notification)
+		{
+			return notification.Errors.Where(e => e.Location.File == textDocument.FilePath).ToArray();
+		}
 
-			if (errorsForThisFile.Any())
-			{
-				// Figure out the smallest span that we have errors for.
-				var newEarliestErrorOffset = errorsForThisFile.Min(e => e.Location.Offset);
-				var newLatestErrorEnd = errorsForThisFile.Max(e => e.Location.Offset + e.Location.Length);
-
-				var startPositionToUse = Math.Min(earliestErrorOffset, newEarliestErrorOffset);
-				var endPositionToUse = Math.Max(latestErrorEnd, newLatestErrorEnd);
-				var lengthToUse = Math.Min(endPositionToUse - startPositionToUse, buffer.CurrentSnapshot.Length - startPositionToUse);
-
-				var handler = this.TagsChanged;
-				if (handler != null)
-					// Use the biggest span that includes boh previous and new errors, so we correctly remove any that are gone.
-					handler(this, new SnapshotSpanEventArgs(new SnapshotSpan(buffer.CurrentSnapshot, startPositionToUse, lengthToUse)));
-
-				// Remember for next time round.
-				earliestErrorOffset = newEarliestErrorOffset;
-				latestErrorEnd = newLatestErrorEnd;
-			}
-			else
-			{
-				// No new errors, so just invalidate the old ones.
-
-				var startPositionToUse = earliestErrorOffset;
-				var endPositionToUse = Math.Min(latestErrorEnd, buffer.CurrentSnapshot.Length);
-				var lengthToUse = Math.Min(endPositionToUse - startPositionToUse, buffer.CurrentSnapshot.Length - startPositionToUse);
-
-
-				var handler = this.TagsChanged;
-				if (handler != null)
-					// Use the biggest span that includes boh previous and new errors, so we correctly remove any that are gone.
-					handler(this, new SnapshotSpanEventArgs(new SnapshotSpan(buffer.CurrentSnapshot, earliestErrorOffset, lengthToUse)));
-
-				earliestErrorOffset = 0;
-				latestErrorEnd = 0;
-			}
+		protected override Tuple<int, int> GetOffsetAndLength(AnalysisError data)
+		{
+			return Tuple.Create(data.Location.Offset, data.Location.Length);
 		}
 	}
 }
