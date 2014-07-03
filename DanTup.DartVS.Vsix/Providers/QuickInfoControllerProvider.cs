42716ba20fc71ebf62b32254a7a80cbd27817c32
Danny Tuppeny <danny@tuppeny.com>
2014-07-03 17:17:15 +0100

    Initial tooltip implementation.
    Maybe buggy; feels a bit hacky to make it Async :-/

---
+++ DanTup.DartVS.Vsix/Providers/QuickInfoControllerProvider.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DanTup.DartVS.Providers
+{
+	[Export(typeof(IIntellisenseControllerProvider))]
+	[ContentType(DartContentTypeDefinition.DartContentType)]
+	class QuickInfoControllerProvider : IIntellisenseControllerProvider
+	{
+		[Import]
+		internal IQuickInfoBroker QuickInfoBroker { get; set; }
+
+		public IIntellisenseController TryCreateIntellisenseController(ITextView textView, IList<ITextBuffer> subjectBuffers)
+		{
+			return new QuickInfoController(textView, subjectBuffers, this);
+		}
+	}
+
+	class QuickInfoController : IIntellisenseController
+	{
+		ITextView textView;
+		IList<ITextBuffer> subjectBuffers;
+		QuickInfoControllerProvider provider;
+		IQuickInfoSession session;
+
+		public QuickInfoController(ITextView textView, IList<ITextBuffer> subjectBuffers, QuickInfoControllerProvider provider)
+		{
+			this.textView = textView;
+			this.subjectBuffers = subjectBuffers;
+			this.provider = provider;
+
+			textView.MouseHover += this.OnTextViewMouseHover;
+		}
+
+		private void OnTextViewMouseHover(object sender, MouseHoverEventArgs e)
+		{
+			// I don't really know what this is all for; but it's in the MSDN sample...
+			// http://msdn.microsoft.com/en-us/library/vstudio/ee197646(v=vs.120).aspx
+			SnapshotPoint? point = textView.BufferGraph.MapDownToFirstMatch(new SnapshotPoint(textView.TextSnapshot, e.Position), PointTrackingMode.Positive, snapshot => subjectBuffers.Contains(snapshot.TextBuffer), PositionAffinity.Predecessor);
+
+			if (point != null)
+			{
+				ITrackingPoint triggerPoint = point.Value.Snapshot.CreateTrackingPoint(point.Value.Position,
+				PointTrackingMode.Positive);
+
+				if (!provider.QuickInfoBroker.IsQuickInfoActive(textView))
+				{
+					session = provider.QuickInfoBroker.TriggerQuickInfo(textView, triggerPoint, true);
+				}
+			}
+		}
+
+		public void ConnectSubjectBuffer(ITextBuffer subjectBuffer)
+		{
+		}
+
+		public void Detach(ITextView txt)
+		{
+			if (textView == txt)
+			{
+				textView.MouseHover -= this.OnTextViewMouseHover;
+				textView = null;
+			}
+		}
+
+		public void DisconnectSubjectBuffer(ITextBuffer subjectBuffer)
+		{
+		}
+	}
+
+}
---
+++ DanTup.DartVS.Vsix/Providers/QuickInfoSourceProvider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Threading.Tasks;
+using DanTup.DartAnalysis;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+namespace DanTup.DartVS.Providers
+{
+	[Export(typeof(IQuickInfoSourceProvider))]
+	[ContentType(DartContentTypeDefinition.DartContentType)]
+	[Name("Test")]
+	[Order(Before = "Default Quick Info Presenter")]
+	class QuickInfoSourceProvider : IQuickInfoSourceProvider
+	{
+		[Import]
+		ITextDocumentFactoryService textDocumentFactory = null;
+
+		[Import]
+		DartAnalysisService analysisService = null;
+
+		public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer buffer)
+		{
+			return new QuickInfoSource(this, buffer, textDocumentFactory, analysisService);
+		}
+	}
+
+	class QuickInfoSource : IQuickInfoSource
+	{
+		QuickInfoSourceProvider provider;
+		ITextBuffer buffer;
+		ITextDocumentFactoryService textDocumentFactory;
+		DartAnalysisService analysisService;
+
+		public QuickInfoSource(QuickInfoSourceProvider provider, ITextBuffer buffer, ITextDocumentFactoryService textDocumentFactory, DartAnalysisService analysisService)
+		{
+			this.provider = provider;
+			this.buffer = buffer;
+			this.textDocumentFactory = textDocumentFactory;
+			this.analysisService = analysisService;
+		}
+
+		public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> quickInfoContent, out ITrackingSpan applicableToSpan)
+		{
+			applicableToSpan = null;
+
+			var triggerPoint = session.GetTriggerPoint(buffer.CurrentSnapshot);
+			if (triggerPoint == null)
+				return;
+
+			ITextDocument doc;
+			if (!textDocumentFactory.TryGetTextDocument(buffer, out doc))
+				return;
+
+			quickInfoContent.Add("Loading...");
+			// TODO: Make this real; not just next 1 char
+			applicableToSpan = buffer.CurrentSnapshot.CreateTrackingSpan(triggerPoint.Value.Position, 1, SpanTrackingMode.EdgeInclusive);
+
+			var hoverTask = analysisService.GetHover(doc.FilePath, triggerPoint.Value.Position);
+			hoverTask.ContinueWith(hovers =>
+			{
+				string tooltipData = null;
+				if (hovers.Result.Length > 0 && hovers.Result[0] != null)
+					tooltipData = hovers.Result[0].elementDescription ?? hovers.Result[0].parameter;
+
+				if (tooltipData != null)
+					session.QuickInfoContent[0] = tooltipData;
+				else
+					session.Dismiss();
+			}, TaskScheduler.FromCurrentSynchronizationContext()); // TODO: Without this, Dismiss doesn't work; but is this a good way to do it?
+		}
+
+		public void Dispose()
+		{
+			GC.SuppressFinalize(true);
+		}
+	}
+}
