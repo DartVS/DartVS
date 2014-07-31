a3cab89b56249adae614a5bf7cf585acdda2e89c
Danny Tuppeny <danny@tuppeny.com>
2014-07-31 19:26:05 +0100

    Added in support for calling external formatter.
    Doesn't actually format yet, but does capture buffer and maintain caret
    position (roughly). Issue #26.

---
+++ DanTup.DartAnalysis/DartFormatter.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+
+namespace DanTup.DartAnalysis
+{
+	/// <summary>
+	/// Temporary SDK-based formatter, until the Analysis Service provides this functionality.
+	/// </summary>
+	public class DartFormatter
+	{
+		public string FormatText(string text)
+		{
+			var tempFilename = string.Format("{0}.dart", Path.GetRandomFileName());
+			var tempFileLocation = Path.Combine(Path.GetTempPath(), tempFilename);
+
+			try
+			{
+				File.WriteAllText(tempFileLocation, text);
+
+				// TODO: Call SDK formatter
+				return text;
+			}
+			finally
+			{
+				try
+				{
+					File.Delete(tempFileLocation);
+				}
+				catch { }
+			}
+
+		}
+	}
+}
--- DanTup.DartVS.Vsix/Formatting/DartFormatDocument.cs
+++ DanTup.DartVS.Vsix/Formatting/DartFormatDocument.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio;
+﻿using DanTup.DartAnalysis;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.TextManager.Interop;
@@ -7,6 +8,8 @@ namespace DanTup.DartVS
 {
 	class DartFormatDocument : DartOleCommandTarget<VSConstants.VSStd2KCmdID>
 	{
+		DartFormatter formatter = new DartFormatter();
+
 		public DartFormatDocument(ITextDocumentFactoryService textDocumentFactory, IVsTextView textViewAdapter, IWpfTextView textView, DartAnalysisService analysisService)
 			: base(textDocumentFactory, textViewAdapter, textView, analysisService, VSConstants.VSStd2KCmdID.FORMATDOCUMENT)
 		{
@@ -14,7 +17,22 @@ namespace DanTup.DartVS
 
 		protected override void Exec()
 		{
-			// TODO: Implement me
+			// Get the current state of the document (we can't format on-disk, as it might not have been saved).
+			var fileContents = textView.TextSnapshot.GetText();
+			var caretLine = textView.TextSnapshot.GetLineNumberFromPosition(textView.Caret.Position.BufferPosition.Position);
+
+			// Call the formatter.
+			var formattedFileContents = formatter.FormatText(fileContents);
+
+			// Create a span that is the entire document, since we're going to replace it.
+			var entireDocumentSpan = new Span(0, textView.TextSnapshot.TextBuffer.CurrentSnapshot.Length);
+
+			// Replace the document with the formatted version.
+			textView.TextSnapshot.TextBuffer.Replace(entireDocumentSpan, formattedFileContents);
+
+			// Set the caret back to the correct line if it isn't a hihger line number than we now have.
+			if (caretLine < textView.TextSnapshot.LineCount)
+				textView.Caret.MoveTo(textView.TextSnapshot.GetLineFromLineNumber(caretLine).Start);
 		}
 	}
 }
