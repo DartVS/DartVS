80173e0d7f30cd09e8e2992a50c7f77eaf11ec47
Danny Tuppeny <danny@tuppeny.com>
2014-08-08 18:15:49 +0100

    Fix typo!

--- UpdateSnapshot.cmd
+++ UpdateSnapshot.cmd
@@ -1,2 +1,2 @@
-call dart2js.bat --output-type=dart --categories=Server "M:\Coding\Applications\Dart\dart\pkg\analysis_server\bin\server.dart" -o "Dart\AnalysisServer2.dart"
+call dart2js.bat --output-type=dart --categories=Server "M:\Coding\Applications\Dart\dart\pkg\analysis_server\bin\server.dart" -o "Dart\AnalysisServer.dart"
 del Dart\AnalysisServer.dart.deps
