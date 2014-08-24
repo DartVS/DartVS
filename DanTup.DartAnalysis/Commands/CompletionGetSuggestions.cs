00a42c8044f46ac99a59109e539680cc5d4ad0b6
Danny Tuppeny <danny@tuppeny.com>
2014-08-24 17:13:33 +0100

    Begin implementing Completions.

---
+++ DanTup.DartAnalysis/Commands/CompletionGetSuggestions.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DanTup.DartAnalysis.Json;
+
+namespace DanTup.DartAnalysis
+{
+	public static class CompletionGetSuggestionsImplementation
+	{
+		public static async Task<string> GetSuggestions(this DartAnalysisService service, string file, int offset)
+		{
+			var request = new CompletionGetSuggestionsRequest
+			{
+				File = file,
+				Offset = offset
+			};
+
+			var response = await service.Service
+				.Send(new CompletionGetSuggestions(request))
+				.ConfigureAwait(continueOnCapturedContext: false);
+
+			return response.result.Id;
+		}
+	}
+}
