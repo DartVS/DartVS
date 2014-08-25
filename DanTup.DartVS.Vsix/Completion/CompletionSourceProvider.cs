a9b2c4046322e9eef4709db71e7e3833c9277b0d
Danny Tuppeny <danny@tuppeny.com>
2014-08-25 15:19:28 +0100

    Added some (currently broken) code-completion code.

--- DanTup.DartVS.Vsix/Completion/CompletionSourceProvider.cs
+++ DanTup.DartVS.Vsix/Completion/CompletionSourceProvider.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Threading.Tasks;
+using DanTup.DartAnalysis.Json;
 using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Utilities;
@@ -47,14 +49,49 @@ namespace DanTup.DartVS
 			if (triggerPoint == null)
 				return;
 
+			ITextDocument doc;
+			if (!textDocumentFactory.TryGetTextDocument(buffer, out doc))
+				return;
+
 			var applicableTo = buffer.CurrentSnapshot.CreateTrackingSpan(new SnapshotSpan(triggerPoint.Value, 1), SpanTrackingMode.EdgeInclusive);
 
 			var completions = new ObservableCollection<Completion>();
-			completions.Add(new Completion("Something1"));
-			completions.Add(new Completion("Something2"));
-			completions.Add(new Completion("Something3"));
+			completions.Add(new Completion("Hard-coded..."));
 
-			completionSets.Add(new CompletionSet("All", "All", applicableTo, completions, Enumerable.Empty<Completion>()));
+			var completionSet = new CompletionSet("All", "All", applicableTo, Enumerable.Empty<Completion>(), completions);
+			completionSets.Add(completionSet);
+
+			Task.Run(async () =>
+			{
+				await Task.Delay(1000);
+				completions.Add(new Completion("Danny"));
+				session.Start();
+			});
+
+			//// Kick of async request to update the results.
+			//analysisService
+			//	.GetSuggestions(doc.FilePath, triggerPoint.Value.Position)
+			//	.ContinueWith(completion =>
+			//	{
+			//		var completionID = completion.Result;
+			//		IDisposable subscription = null;
+			//		subscription = analysisService
+			//			.CompletionResultsNotification
+			//			.Where(c => c.Id == completionID)
+			//			.Subscribe(c => UpdateCompletionResults(subscription, completions, c));
+			//	});
+		}
+
+		void UpdateCompletionResults(IDisposable subscription, ObservableCollection<Completion> completions, CompletionResultsNotification results)
+		{
+			foreach (var result in results.Results)
+			{
+				completions.Add(new Completion(result.Completion)); //, result.Completion, result.DocComplete, null, null));
+			}
+
+			// If we were notified that this was the last set of results; remove the subscription.
+			if (results.Last)
+				subscription.Dispose();
 		}
 
 		public void Dispose()
