735b9ca023a4acce751468a2ce7812f411c8a3e6
Sam Harwell <sam@tunnelvisionlabs.com>
2014-10-15 18:23:52 -0500

    Export classification type definitions for each highlight region type (fixes #63)

--- DanTup.DartVS.Vsix/Taggers/ClassificationTagger.cs
+++ DanTup.DartVS.Vsix/Taggers/ClassificationTagger.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
 using DanTup.DartAnalysis;
 using DanTup.DartAnalysis.Json;
-using Microsoft.VisualStudio.Language.StandardClassification;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Text.Tagging;
@@ -36,59 +34,22 @@ namespace DanTup.DartVS
 
 	class ClassificationTagger : AnalysisNotificationTagger<ClassificationTag, HighlightRegion, AnalysisHighlightsNotification>
 	{
-		IDictionary<HighlightRegionType, IClassificationType> classificationMapping;
+		IClassificationType[] classificationMapping;
 
 		public ClassificationTagger(ITextBuffer buffer, ITextDocumentFactoryService textDocumentFactory, DartAnalysisServiceFactory analysisServiceFactory, IClassificationTypeRegistryService typeService)
 			: base(buffer, textDocumentFactory, analysisServiceFactory)
 		{
-			// Mapping of Analysis Server's HighlightRegionTypes to the VS Classifications we wish to give them.
-			classificationMapping = new Dictionary<HighlightRegionType, IClassificationType>
-			{
-				// TODO: Replace any 'Other' with something more appropriate (eg. NaturalLanguage/FormatLanguage or more specific).
-				// NOTE: We must map *everything* here, because Tooltips rely on these tags to calculate applicableTo spans immediately.
-				{ HighlightRegionType.Annotation, typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolReference) },
-				{ HighlightRegionType.BuiltIn, typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword) },
-				{ HighlightRegionType.Class, typeService.GetClassificationType(PredefinedClassificationTypeNames.SymbolReference) },
-				{ HighlightRegionType.CommentBlock, typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment) },
-				{ HighlightRegionType.CommentDocumentation, typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment) },
-				{ HighlightRegionType.CommentEndOfLine, typeService.GetClassificationType(PredefinedClassificationTypeNames.Comment) },
-				{ HighlightRegionType.Constructor, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Directive, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.DynamicType, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Field, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.FieldStatic, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Function, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.FunctionDeclaration, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.FunctionTypeAlias, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.GetterDeclaration, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.IdentifierDefault, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.ImportPrefix, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Keyword, typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword) },
-				{ HighlightRegionType.LiteralBoolean, typeService.GetClassificationType(PredefinedClassificationTypeNames.Keyword) },
-				{ HighlightRegionType.LiteralDouble, typeService.GetClassificationType(PredefinedClassificationTypeNames.Number) },
-				{ HighlightRegionType.LiteralInteger, typeService.GetClassificationType(PredefinedClassificationTypeNames.Number) },
-				{ HighlightRegionType.LiteralList, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.LiteralMap, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.LiteralString, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.LocalVariable, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.LocalVariableDeclaration, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Method, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.MethodDeclaration, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.MethodDeclarationStatic, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.MethodStatic, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.Parameter, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.SetterDeclaration, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.TopLevelVariable, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.TypeNameDynamic, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-				{ HighlightRegionType.TypeParameter, typeService.GetClassificationType(PredefinedClassificationTypeNames.Other) },
-			};
+			Array values = Enum.GetValues(typeof(HighlightRegionType));
+			classificationMapping = new IClassificationType[values.Length];
+			for (int i = 0; i < classificationMapping.Length; i++)
+				classificationMapping[i] = typeService.GetClassificationType(DartConstants.ContentType + ((HighlightRegionType)i).ToString());
 
 			this.Subscribe();
 		}
 
 		protected override ITagSpan<ClassificationTag> CreateTag(HighlightRegion highlight)
 		{
-			return new TagSpan<ClassificationTag>(new SnapshotSpan(buffer.CurrentSnapshot, highlight.Offset, highlight.Length), new ClassificationTag(classificationMapping[highlight.Type]));
+			return new TagSpan<ClassificationTag>(new SnapshotSpan(buffer.CurrentSnapshot, highlight.Offset, highlight.Length), new ClassificationTag(classificationMapping[(int)highlight.Type]));
 		}
 
 		protected override async Task<IDisposable> SubscribeAsync(Action<AnalysisHighlightsNotification> updateSourceData)
@@ -104,7 +65,7 @@ namespace DanTup.DartVS
 			if (classificationMapping == null)
 				return new HighlightRegion[0];
 
-			return notification.Regions.Where(h => classificationMapping.ContainsKey(h.Type)).ToArray();
+			return notification.Regions.Where(h => h.Type >= 0 && (int)h.Type < classificationMapping.Length).ToArray();
 		}
 
 		protected override Tuple<int, int> GetOffsetAndLength(HighlightRegion data)
