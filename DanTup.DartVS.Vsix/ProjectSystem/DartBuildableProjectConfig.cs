af7b937872fbcb446a69bb2d989f4a9f83ff6c4e
Sam Harwell <sam@tunnelvisionlabs.com>
2014-10-15 22:50:28 -0500

    Remove the Build, Rebuild, and Clean commands from the project context menu (fixes #62)

---
+++ DanTup.DartVS.Vsix/ProjectSystem/DartBuildableProjectConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Project;
+
+namespace DanTup.DartVS.ProjectSystem
+{
+	internal class DartBuildableProjectConfig : BuildableProjectConfig
+	{
+		public DartBuildableProjectConfig(ProjectConfig config)
+			: base(config)
+		{
+		}
+
+		public override int QueryStartBuild(uint options, int[] supported, int[] ready)
+		{
+			if (supported != null && supported.Length > 0)
+				supported[0] = 0;
+
+			if (ready != null && ready.Length > 0)
+				ready[0] = 0;
+
+			return VSConstants.S_OK;
+		}
+
+		public override int QueryStartClean(uint options, int[] supported, int[] ready)
+		{
+			if (supported != null && supported.Length > 0)
+				supported[0] = 0;
+
+			if (ready != null && ready.Length > 0)
+				ready[0] = 0;
+
+			return VSConstants.S_OK;
+		}
+	}
+}
--- DanTup.DartVS.Vsix/ProjectSystem/DartProjectConfig.cs
+++ DanTup.DartVS.Vsix/ProjectSystem/DartProjectConfig.cs
@@ -2,9 +2,12 @@
 {
     using Microsoft.VisualStudio;
     using Microsoft.VisualStudio.Project;
+    using Microsoft.VisualStudio.Shell.Interop;
 
     public class DartProjectConfig : ProjectConfig
     {
+        private DartBuildableProjectConfig buildableCfg;
+
         internal DartProjectConfig(DartProjectNode project, string configuration, string platform)
             : base(project, configuration, platform)
         {
@@ -26,6 +29,15 @@
         public override int QueryDebugLaunch(uint flags, out int fCanLaunch)
         {
             fCanLaunch = 0;
+            return VSConstants.S_OK;
+        }
+
+        public override int get_BuildableProjectCfg(out IVsBuildableProjectCfg pb)
+        {
+            if (buildableCfg == null)
+                buildableCfg = new DartBuildableProjectConfig(this);
+
+            pb = buildableCfg;
             return VSConstants.S_OK;
         }
     }
