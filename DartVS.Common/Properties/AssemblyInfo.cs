09086e7f9d2543aa632512e743ae4ea555a0ae29
Danny Tuppeny <danny@tuppeny.com>
2014-10-18 11:42:39 +0100

    Moved some common AssemblyInfo strings to SharedAssemblyInfo.

--- DartVS.Common/Properties/AssemblyInfo.cs
+++ DartVS.Common/Properties/AssemblyInfo.cs
@@ -1,8 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DartVS: Visual Studio support for Google's Dart")]
 [assembly: AssemblyProduct("DartVS: Visual Studio support for Google's Dart")]
-[assembly: AssemblyCompany("Danny Tuppeny")]
-[assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
-[assembly: ComVisible(false)]
+
