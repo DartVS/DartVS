0918ac9fd74543b9f8a2169faed39fbdb7ae2124
Danny Tuppeny <danny@tuppeny.com>
2014-10-18 11:40:44 +0100

    Removed some DanTup prefixes from assembly info.

--- DanTup.DartVS.Vsix/Properties/AssemblyInfo.cs
+++ DanTup.DartVS.Vsix/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DanTup's DartVS: Visual Studio support for Google's Dart (VSIX)")]
-[assembly: AssemblyProduct("DanTup's DartVS: Visual Studio support for Google's Dart")]
+[assembly: AssemblyTitle("DartVS: Visual Studio support for Google's Dart (VSIX)")]
+[assembly: AssemblyProduct("DartVS: Visual Studio support for Google's Dart")]
 [assembly: AssemblyCompany("Danny Tuppeny")]
 [assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
 [assembly: ComVisible(false)]
