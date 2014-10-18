003d91018d67210811300f3495f72b53d1317589
Danny Tuppeny <danny@tuppeny.com>
2014-10-18 11:28:02 +0100

    Added empty DartVS.Common project.
    This is to house things that are shared across all projects.

---
+++ DartVS.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DanTup's DartVS: Visual Studio support for Google's Dart")]
+[assembly: AssemblyProduct("DanTup's DartVS: Visual Studio support for Google's Dart")]
+[assembly: AssemblyCompany("Danny Tuppeny")]
+[assembly: AssemblyCopyright("Copyright Danny Tuppeny © 2014")]
+[assembly: ComVisible(false)]
